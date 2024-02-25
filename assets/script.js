26d5d734933fc981dea42707fb82e578a899f371
xinyizhangx <136768650+xinyizhangx@users.noreply.github.com>
2024-02-24 20:15:02 -0500

    Create script.js

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+let button = document.querySelector("#btn-enter");
+
+button.onclick = () => {
+  alert(
+    "🤗 Enjoy your click journey. Each circle is the key to a new world. Are you ready to explore your exclusive tranquility? 🤗 "
+  );
+};
