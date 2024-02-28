dc6776f383eec0c4fa994fda719373b87e179e5e
xinyizhangx <136768650+xinyizhangx@users.noreply.github.com>
2024-02-28 05:45:54 -0500

    ...
    alot

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,20 @@
 let button = document.querySelector("#btn-enter");
 
 button.onclick = () => {
-  alert(
-    "🤗 Enjoy your click journey. Each circle is the key to a new world. Are you ready to explore your exclusive tranquility? 🤗 "
-  );
+  if (!document.querySelector(".warning")) {
+    let dialog = document.createElement("div");
+    let span = document.createElement("span");
+    let span2 = document.createElement("span");
+    span.innerHTML = `🤗 Welcome to White Noise Oasis. 🤗 Every click is an unexpected journey. Here, each circle is the key to a new world. Are you ready to explore your exclusive tranquility?`;
+    span.classList.add("textAlert");
+    span2.innerHTML = "➡️";
+    dialog.appendChild(span);
+    dialog.appendChild(span2);
+    dialog.className = "warning";
+    span2.className = "close-btn";
+    span2.onclick = () => {
+      document.body.removeChild(dialog);
+    };
+    document.body.appendChild(dialog);
+  }
 };
